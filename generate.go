4eb2d71c98117c97d05236277127c9e9234811b6
CDN18 <cardinal@codeword.info>
2024-08-05 13:15:28 +0800

    add generate.go

---
+++ generate.go
@@ -0,0 +1,3 @@
+package main
+
+//go:generate go run github.com/go-swagger/go-swagger/cmd/swagger generate client --spec https://raw.githubusercontent.com/superseriousbusiness/gotosocial/main/docs/api/swagger.yaml
