e9a79eca4d14eee1faf9eda851966ed31d3d5f0d
CDN18 <CDN18@users.noreply.github.com>
2024-12-02 19:07:42 +0000

    update generated client

--- models/instance_v1.go
+++ models/instance_v1.go
@@ -28,6 +28,9 @@ type InstanceV1 struct {
 	// New account registrations require admin approval.
 	ApprovalRequired bool `json:"approval_required,omitempty"`
 
+	// Custom CSS for the instance.
+	CustomCSS string `json:"custom_css,omitempty"`
+
 	// Whether or not instance is running in DEBUG mode. Omitted if false.
 	Debug bool `json:"debug,omitempty"`
 
